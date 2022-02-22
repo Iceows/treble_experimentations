4f5eb8bb89f0cdf23483a8cf4866f37bdb8b441b
Raphael Mounier <mounierr07@gmail.com>
2022-02-22 15:26:54 +0100

    Add support of custom vendor hardware overlay and parter gms

--- apply-patches.sh
+++ apply-patches.sh
@@ -4,18 +4,17 @@ set -e
 
 patches="$(readlink -f -- $1)"
 
-for project in $(cd $patches/patches; echo *);do
+shopt -s nullglob
+for project in $(cd $patches; echo *);do
 	p="$(tr _ / <<<$project |sed -e 's;platform/;;g')"
 	[ "$p" == build ] && p=build/make
-	repo sync -l --force-sync $p || continue
+	[ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay
+	[ "$p" == vendor/partner/gms ] && p=vendor/partner_gms
+	[ "$p" == external/harfbuzz/ng ] && p=external/harfbuzz_ng
+	echo	
 	pushd $p
 	git clean -fdx; git reset --hard
-	for patch in $patches/patches/$project/*.patch;do
-		#Check if patch is already applied
-		if patch -f -p1 --dry-run -R < $patch > /dev/null;then
-			continue
-		fi
-
+	for patch in $patches/$project/*.patch;do
 		if git apply --check $patch;then
 			git am $patch
 		elif patch -f -p1 --dry-run < $patch > /dev/null;then
@@ -26,6 +25,7 @@ for project in $(cd $patches/patches; echo *);do
 			git am --continue
 		else
 			echo "Failed applying $patch"
+			exit 1
 		fi
 	done
 	popd
